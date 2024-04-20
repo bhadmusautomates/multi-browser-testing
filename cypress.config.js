b977d91d39493a29aca2315ab6bc134b3f0a2070
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-04-20 10:44:57 +0100

    adjusted run command

--- cypress.config.js
+++ cypress.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
     experimentalRunAllSpecs: true,
     specPattern: '**/*.feature',
     // env: {
-    //   "MAILSLURP_API_KEY": "2fc2e35c789884f31b43bf8db3ce62ee4772947d802bdec20530b04e54fb21f4"
+    //   "MAILSLURP_API_KEY": "50dde13318eef36ffd69ee6974950ce2b2b5ddb1ff125a5ef4f0f560ea2c78f0"
     // },
     setupNodeEvents,
   },
